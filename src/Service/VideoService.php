acda1b3a4a21d999c7ee0d9fbdcaa8a287092a22
Mamadou MARÉGA <hamshakour93@gmail.com>
2024-11-14 22:50:56 +0100

    Added logic of functions

--- src/Service/VideoService.php
+++ src/Service/VideoService.php
@@ -3,25 +3,94 @@
 namespace App\Service;
 
 use App\Entity\Video;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Exception;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use function PHPUnit\Framework\throwException;
 
 class VideoService
 {
-    public function __construct()
+    protected EntityManagerInterface $entityManager;
+    protected ParameterBagInterface $parameterBag;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param ParameterBagInterface $parameterBag
+     */
+    public function __construct(EntityManagerInterface $entityManager, ParameterBagInterface $parameterBag)
     {
+        $this
+            ->setEntityManager($entityManager)
+            ->setParameterBag($parameterBag)
+        ;
     }
 
-    public function handleVideoForm(FormInterface $videoForm)
+    /**
+     * @return EntityManagerInterface
+     */
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @return $this
+     */
+    public function setEntityManager(EntityManagerInterface $entityManager): self
+    {
+        $this->entityManager = $entityManager;
+
+        return $this;
+    }
+
+    /**
+     * @return ParameterBagInterface
+     */
+    public function getParameterBag(): ParameterBagInterface
+    {
+        return $this->parameterBag;
+    }
+
+    /**
+     * @param ParameterBagInterface $parameterBag
+     * @return $this
+     */
+    public function setParameterBag(ParameterBagInterface $parameterBag): self
+    {
+        $this->parameterBag = $parameterBag;
+
+        return $this;
+    }
+
+    /**
+     * @param FormInterface $videoForm
+     * @return JsonResponse
+     */
+    public function handleVideoForm(FormInterface $videoForm) : JsonResponse
     {
         if ($videoForm->isSubmitted()) {
            $this->handleValidForm($videoForm);
         } else {
             $this->handleInvalidForm($videoForm);
         }
+
+        return new JsonResponse([]);
     }
 
-    public function handleValidForm(FormInterface $videoForm)
+
+    /**
+     * @param FormInterface $videoForm
+     * @return JsonResponse
+     */
+    public function handleValidForm(FormInterface $videoForm): JsonResponse
     {
         /** @var Video $video */
         $video = $videoForm->getData();
@@ -31,11 +100,43 @@ class VideoService
 
         /** @var UploadedFile $videoFile */
         $videoFile = $videoForm->get('videoFile')->getData();
+
+        $thumnailDirectory = $this->getParameterBag()->get('thumbnails.upload_directory');
+        $videoDirectory = $this->getParameterBag()->get('videos.upload_directory');
+
+        if (null !== $uploadThumbnail) {
+            $newFileName = $this->renameUploadedFile($uploadThumbnail, $thumnailDirectory);
+            $video->setThumbnail($newFileName);
+        }
+
+        $newFileName = $this->renameUploadedFile($videoFile, $videoDirectory);
+        $video->setThumbnail($newFileName);
+
+        $em = $this->getEntityManager();
+
+        $em->persist($video);
+        $em->flush();
+
+        return new JsonResponse(
+            [
+                'code' => Video::VIDEO_ADDED_SUCCESSFULLY,
+                'html' => ''
+            ]
+        );
     }
 
+    /**
+     * @param FormInterface $videoForm
+     * @return JsonResponse
+     */
     public function handleInvalidForm(FormInterface $videoForm)
     {
-
+        return new JsonResponse(
+            [
+                'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'html' => ''
+            ]
+        );
     }
 
     /**
