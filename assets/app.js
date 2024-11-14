a9953048c60cb2a8f59556b59cd8021410e6df8e
Mamadou MARÉGA <hamshakour93@gmail.com>
2024-11-14 22:41:47 +0100

    Added form processing

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,26 @@
 import './styles/app.css';
+
+const formVideo = document.getElementById('formVideo');
+
+formVideo.addEventListener('submit', function (event)  {
+    event.preventDefault();
+
+    fetch(this.action, {
+        body: new FormData(event.target),
+        method: 'POST',
+
+    }).then(r =>{
+        return r.json();
+    }).then(json => {
+        console.log(json);
+    });
+});
+
+const handleResponse = (response) => {
+    switch(response.code) {
+        case 200:
+            break;
+        case 500:
+            break;
+    }
+};
